ae8e95080b0b0c5dfc352f9f06ec19965fd83d4c
Brian Lora De Dios <normadd64@gmail.com>
2026-08-18 02:15:35 -0400

    diseño

--- SistemaGestionBibliotecas/Presentacion/Form1.Designer.cs
+++ SistemaGestionBibliotecas/Presentacion/Form1.Designer.cs
@@ -47,18 +47,20 @@
             // label1
             // 
             label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label1.Location = new Point(30, 30);
             label1.Name = "label1";
-            label1.Size = new Size(64, 20);
+            label1.Size = new Size(76, 23);
             label1.TabIndex = 0;
             label1.Text = "Nombre";
             // 
             // label2
             // 
             label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label2.Location = new Point(30, 68);
             label2.Name = "label2";
-            label2.Size = new Size(66, 20);
+            label2.Size = new Size(78, 23);
             label2.TabIndex = 1;
             label2.Text = "Apellido";
             label2.Click += label2_Click;
@@ -66,36 +68,40 @@
             // label3
             // 
             label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label3.Location = new Point(30, 103);
             label3.Name = "label3";
-            label3.Size = new Size(54, 20);
+            label3.Size = new Size(64, 23);
             label3.TabIndex = 2;
             label3.Text = "Correo";
             // 
             // label4
             // 
             label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label4.Location = new Point(30, 135);
             label4.Name = "label4";
-            label4.Size = new Size(59, 20);
+            label4.Size = new Size(63, 20);
             label4.TabIndex = 3;
             label4.Text = "Usuario";
             // 
             // label5
             // 
             label5.AutoSize = true;
-            label5.Location = new Point(30, 168);
+            label5.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.Location = new Point(30, 161);
             label5.Name = "label5";
-            label5.Size = new Size(83, 20);
+            label5.Size = new Size(99, 23);
             label5.TabIndex = 4;
             label5.Text = "Contraseña";
             // 
             // label6
             // 
             label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label6.Location = new Point(30, 204);
             label6.Name = "label6";
-            label6.Size = new Size(31, 20);
+            label6.Size = new Size(36, 23);
             label6.TabIndex = 5;
             label6.Text = "Rol";
             // 
@@ -166,6 +172,7 @@
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(255, 224, 192);
             ClientSize = new Size(800, 450);
             Controls.Add(btnLimpiar);
             Controls.Add(btnGuardar);
@@ -181,8 +188,10 @@
             Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(label1);
+            ForeColor = Color.Black;
             Name = "frmRegistroUsuarios";
             Text = "Registro de Usuarios - Biblioteca ";
+            TransparencyKey = Color.Transparent;
             ResumeLayout(false);
             PerformLayout();
         }
